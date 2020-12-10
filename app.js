7bd6ffb2976da458de0d50e3297fe68d352e10c5
long123war <454725121@qq.com>
2020-12-10 21:33:31 +0800

    12102133

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/Custom', (req, res) => {
+  res.send('Custom')
+})
+app.get('/process', (req, res) => {
+  res.send('process')
+})
 app.listen(port, () => console.log(`Example app listening on port port!`))
