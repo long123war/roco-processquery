8e5a28a612ee48bbdb6b21b3b36f5ec0e716a6c6
long123war <454725121@qq.com>
2020-12-11 00:00:01 +0800

    12102359

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const url = require('url')
 const app = express()
 const port = 8080
-
+// 定制模块列表请求
 app.get('/Custom', (req, res) => {
-  res.send('Custom')
+  // const urlObj = url.parse(req.url ,true);
+  // const query = urlObj.query;
+  let custom = {}
+  let woodenDoor = [
+    "单开门","子母门","双开门"
+  ]
+
+  res.send(urlObj)
 })
+// 木门工艺算法结果请求
 app.get('/process', (req, res) => {
   res.send('process')
 })
