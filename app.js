25079aeec99d8451cc7699b0ae1fbb70f9eb5eb7
spigotRoco <454725121@qq.com>
2020-12-11 17:26:56 +0800

    12111726

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const url = require('url')
-const app = express()
-const port = 8080
+const express = require("express");
+const url = require("url");
+const app = express();
+const port = 8080;
+const wdProcess = require("./moudle/woodenDoor");
 // 定制模块列表请求
-app.get('/Custom', (req, res) => {
+app.get("/Custom", (req, res) => {
   // const urlObj = url.parse(req.url ,true);
   // const query = urlObj.query;
-  let custom = {}
-  let woodenDoor = [
-    "单开门","子母门","双开门"
-  ]
+  let custom = {};
+  let rocoCustom = ["门窗定制", "家具定制", "厨卫定制"];
+  let woodenDoor = ["单开门", "子母门", "双开门"];
 
-  res.send(urlObj)
-})
+  res.send(wdProcess.rocoMenus());
+});
 // 木门工艺算法结果请求
-app.get('/process', (req, res) => {
-  res.send('process')
-})
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.get("/process", (req, res) => {
+  res.send("process");
+});
+app.listen(port, () => console.log(`Example app listening on port port!`));
