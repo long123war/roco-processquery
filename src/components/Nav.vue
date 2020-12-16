ba8ececad79322d46ebcb3e6d173eeaaaef3a855
Cory806 <331284851@qq.com>
2020-12-16 14:02:31 +0800

    修改了结构

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,35 +22,12 @@
             <el-menu-item index="1-2">选项2</el-menu-item>
             <el-menu-item index="1-3">选项3</el-menu-item>
           </el-submenu>
-          <el-submenu index="2">
-            <template slot="title"
-              ><span class="title">我的工作台</span></template
-            >
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title"
-              ><span class="title">我的工作台</span></template
-            >
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-           <template slot="title"
-              ><span class="title">我的工作台</span></template
-            >
-            <el-menu-item index="4-1">选项1</el-menu-item>
-            <el-menu-item index="4-2">选项2</el-menu-item>
-            <el-menu-item index="4-3">选项3</el-menu-item>
-          </el-submenu>
+
         </el-menu>
       </el-header>
       <el-main>
         <div class="banner">
-          <el-carousel height="250px" :arrow="arrow" :autoplay="false">
+          <el-carousel height="250px" :arrow="arrow" autoplay>
             <el-carousel-item v-for="item in imgList" :key="item.url">
               <img :src="item.url" width="100%"/>
             </el-carousel-item>
@@ -60,26 +37,6 @@
       <!-- 页面主体区域 -->
       <el-container class="HolyGrail-body">
         <el-main class="HolyGrail-content">
-          <el-menu  mode="horizontal">
-           <el-tabs :tab-position="tabPosition" style="height: 300px;">
-                <el-tab-pane label="硬装">                   
-                     <h4 class="door">门</h4>        
-                   <el-menu-item>
-                     <a href="https://www.ele.me" target="_blank">室内门</a>
-                     </el-menu-item>
-                       <el-menu-item >
-                     <a href="https://www.ele.me" target="_blank">大门</a>
-                     </el-menu-item>
-                      <el-menu-item >
-                     <a href="https://www.ele.me" target="_blank">移门</a>
-                     </el-menu-item>
-                     <el-menu-item><br><div class="line">123</div></el-menu-item>
-                     </el-tab-pane>
-                <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-                <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-                <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-            </el-tabs>
-          </el-menu> 
         </el-main>
         <nav class="HolyGrail-nav"></nav>
         <aside class="HolyGrail-ads"></aside>
@@ -99,12 +56,24 @@ export default {
         { url: require("../assets/img/3.jpg") }
       ],
       arrow: "never",
-       //随意门方向
-       tabPosition:'left'
+
+      //导航栏数据
+      navList:[]
+      
     };
   },
-  created() {},
+  created() {
+     this.getNavlist()
+  },
   methods: {
+
+      //获取导航栏数据
+     async getNavlist(){
+       const {data:res} = await this.$http.get('Custom')
+       console.log(res)
+      },
+
+
     // 导航栏触发的函数
     handleSelect() {}
   }
@@ -131,28 +100,10 @@ export default {
     min-width: 0px;
     
 }
-//修改随意门的样式
- a:-webkit-any-link {
-    color: #666;
-    cursor: pointer;
-    text-decoration: none;   
-   }
-   .el-menu-item:focus, .el-menu-item:hover {
-    outline: 0;
-   background-color:transparent;   
-}
-.el-menu-item{
-  float: left;
-  padding: 0 5px;
-  margin: 0;
-  height: 30px;
-  line-height: 30px;
-  :hover{
-    color:rgb(230, 33, 41);
-  }
+
   
   
-}
+
 </style>
 
 <style lang="less" scoped>
@@ -191,7 +142,5 @@ export default {
 .el-main{
  padding: 0;
 }
-.door{
-    margin: 10px;
-  }
+
 </style>
