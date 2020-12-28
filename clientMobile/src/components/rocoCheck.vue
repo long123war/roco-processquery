656c0f3892162300b13b742511bf98a283195b84
long123war <454725121@qq.com>
2020-12-28 23:48:53 +0800

    创建了移动端

---
+++ clientMobile/src/components/rocoCheck.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <img class="logo" src="../assets/rocoRogo-red.svg" alt="劳卡全屋定制" />
+  </div>
+</template>
+
+<script>
+export default {
+  created() {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.hello {
+  .logo {
+    margin: 8px 0 11px 0;
+    width: 300px;
+  }
+}
+</style>
---
+++ clientMobile/src/plugins/vant.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import { Icon, Image as VanImage } from "vant";
+
+Vue.use(Icon);
+Vue.use(VanImage);
