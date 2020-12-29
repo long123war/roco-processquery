a0394dc8f3212e9aa1ce83aeccd41d47ed1af93c
spigotRoco <454725121@qq.com>
2020-12-29 17:23:48 +0800

    增加全局样式

--- clientMobile/src/main.js
+++ clientMobile/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "../src/plugins/vant";
+import "../src/assets/global.css";
 
 Vue.config.productionTip = false;
 
